13def4c6ff842372efe9b632b4e67cfeba3ac996
oldmnj <oldmnj@163.com>
2026-07-22 19:52:11 +0800

    Delte Virtual Function Account::deposit

--- include/account.h
+++ include/account.h
@@ -14,7 +14,7 @@ class Account {
 
   public:
     Account(std::int64_t id_, const std::string &owner_, double balance_);
-    virtual void deposit(double amount_);
+    void deposit(double amount_);
     virtual void withdraw(double amount_);
     double getBalance() const;
     std::int64_t getId() const;
